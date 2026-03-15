ace5e20691b08d045753a7284b0e1b5561f3aa75
Badru <badruakfm@gmail.com>
2026-03-15 09:20:54 +0700

    update channel

--- channels.data.js
+++ channels.data.js
@@ -1,4 +1,13 @@
 export const channels = [
     { name: "Baku Hantam", id: "UCCMJRdfvGz95Awe86FPsf3w" },
     { name: "Lord Op", id: "UCxWSbddx9ly26twkLf78jOQ" },
+    { name: "Para Real Men", id: "UCaN1MBfZgT5ZFulxnkFgc7Q" },
+    { name: "Hindustan Times", id: "UCm7lHFkt2yB_WzL67aruVBQ" },
+    { name: "Military Tube Today", id: "UCpcJHYjCybrPcW3TNr6q6pw" },
+    { name: "BBC News", id: "UC16niRr50-MSBwiO3YDb3RA" },
+    { name: "CNN", id: "UCupvZG-5ko_eiXAupbDfxWw" },
+    { name: "The Telegraph", id: "UCPgLNge0xqQHWM5B5EFH9Cg" },
+    
 ];
+
+
